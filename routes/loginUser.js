8ff4af912b4310f31fa15c70cf9b8ba72eb323f8
Michael Roberts <that4wkwardguy@gmail.com>
2018-08-01 12:12:58 -0700

    added user login functionality

--- routes/loginUser.js
+++ routes/loginUser.js
@@ -1,7 +1,54 @@
+const User = require("./../schemas/UserSchema");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const secretOrKey = require("./../config/secrets").secret;
 
-
-const loginUser = (requestAnimationFrame, res) => {
-
+const loginUser = (req, res) => {
+  let error = { error: undefined };
+  if (!req.body.username) {
+    error.error = "no username provided";
+    res.json(error);
+  } else if (!req.body.password) {
+    error.error = "no password provided";
+    res.json(error);
+  } else {
+    User.findOne({ username: req.body.username }, (err, user) => {
+      if (err) {
+        err.error = err;
+        res.json(error);
+      } else if (!user) {
+        error.error = "invalid username or password";
+        res.json(error);
+      } else {
+        bcrypt.compare(req.body.password, user.password, (err, match) => {
+          if (err) {
+            err.error = err;
+            res.json(error);
+          } else if (!match) {
+            error.error = "invalid username or password";
+            res.json(error);
+          } else {
+            jwt.sign(
+              {
+                username: user.username,
+                password: user.password,
+                exp: Math.floor(Date.now() / 1000) + 31557600
+              },
+              secretOrKey,
+              (err, token) => {
+                if (err) {
+                  error.error = err;
+                  res.json(error);
+                } else {
+                  res.json(token);
+                }
+              }
+            );
+          }
+        });
+      }
+    });
+  }
 };
 
 module.exports = loginUser;
